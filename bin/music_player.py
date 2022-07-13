adb024c7dfcd36d9b3df61845ff6ba8d8193fa7b
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-07-13 05:26:47 +0000

    Fixing linting issues

--- bin/music_player.py
+++ bin/music_player.py
@@ -1,1 +1,8 @@
-#!/usr
+#!/usr/bin/env python3
+import os
+import subprocess
+from hashlib import md5
+
+# Configuration
+music_dir = "/Volumes/BrahmaSSD/music/AiGen"
+output_file = "/tmp/combined_playlist.wa
