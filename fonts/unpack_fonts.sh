3cc55cce9d0bd6a1f410d7ef2668751574c73a60
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-08-31 05:54:17 +0000

    Not sure

--- fonts/unpack_fonts.sh
+++ fonts/unpack_fonts.sh
@@ -6,4 +6,6 @@
 cabextract --lowercase --directory=cab-contents trebuc32.exe
 
 cp -f cab-contents/trebuc.ttf TrebuchetMS.ttf
-cp -f cab-contents
+cp -f cab-contents/trebucbd.ttf TrebuchetMSBold.ttf
+rm -rf cab-contents
+
---
+++ scripts/compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+set -e
+
+# Copy NDI library to the local directory
+#if [ ! -f "lib
