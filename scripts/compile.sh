22e09da7340a023b85549a3b0914fb3112cc1155
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-08-31 07:41:05 +0000

    v2.2.1

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,4 +3,6 @@
 set -e
 
 # Copy NDI library to the local directory
-#if [ ! -f "lib
+#if [ ! -f "libndi.dylib" ]; then
+#    curl -L https://github.com/nariakiiwatani/ofxNDI/raw/master/libs/NDI/lib/osx/x64/libndi.4.dylib --output libndi.dylib
+#f
