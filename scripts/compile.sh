074589bfd9e1954228473ef1705f3a59e8209d88
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-09-02 18:52:40 +0000

    Added sample data

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,4 +5,12 @@ set -e
 # Copy NDI library to the local directory
 #if [ ! -f "libndi.dylib" ]; then
 #    curl -L https://github.com/nariakiiwatani/ofxNDI/raw/master/libs/NDI/lib/osx/x64/libndi.4.dylib --output libndi.dylib
-#f
+#fi
+
+# Point to NDI path
+if [ "$DYLD_LIBRARY_PATH" = "" ]; then
+    export DYLD_LIBRARY_PATH=`pwd`:/usr/local/lib:$DYLD_LIBRARY_PATH
+fi
+
+## Build release version
+cargo b
