1364d51d19748dcb70b69ce2bf376f6036443e7b
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-09-09 14:44:52 +0000

    EOD commit

--- scripts/mpeg_analyzer.sh
+++ scripts/mpeg_analyzer.sh
@@ -2,4 +2,5 @@
 #
 sudo DYLD_LIBRARY_PATH=`pwd`:/usr/local/lib:$DYLD_LIBRARY_PATH target/release/rsllm \
     --daemon  \
-    --system-prompt "You are an expert  MpegTS Analyzer that can decode and decipher hex packets and general statistics of MpegTS. You report the status and health of the stream, alerting when a
+    --system-prompt "You are an expert  MpegTS Analyzer that can decode and decipher hex packets and general statistics of MpegTS. You report the status and health of the stream, alerting when anything is wrong. Do not make up stats, only use what you can verifiably see in the context." \
+    --query "Analyze the timeline shown in the historical context of mpeg 
