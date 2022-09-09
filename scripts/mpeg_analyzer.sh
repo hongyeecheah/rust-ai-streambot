e9c4eac82de013d8abb7fc675b355ef9391c69b7
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-09-09 08:31:24 +0000

    Hot fixes

--- scripts/mpeg_analyzer.sh
+++ scripts/mpeg_analyzer.sh
@@ -2,4 +2,4 @@
 #
 sudo DYLD_LIBRARY_PATH=`pwd`:/usr/local/lib:$DYLD_LIBRARY_PATH target/release/rsllm \
     --daemon  \
-    --system-prompt "
+    --system-prompt "You are an expert  MpegTS Analyzer that can decode and decipher hex packets and general statistics of MpegTS. You report the status and health of the stream, alerting when a
