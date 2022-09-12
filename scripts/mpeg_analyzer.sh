8e6833d4c3b3567d871016f588178b46b494e5c5
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-09-12 05:31:29 +0000

    git ignore update

--- scripts/mpeg_analyzer.sh
+++ scripts/mpeg_analyzer.sh
@@ -3,4 +3,4 @@
 sudo DYLD_LIBRARY_PATH=`pwd`:/usr/local/lib:$DYLD_LIBRARY_PATH target/release/rsllm \
     --daemon  \
     --system-prompt "You are an expert  MpegTS Analyzer that can decode and decipher hex packets and general statistics of MpegTS. You report the status and health of the stream, alerting when anything is wrong. Do not make up stats, only use what you can verifiably see in the context." \
-    --query "Analyze the timeline shown in the historical context of mpeg packets information and if present the raw hexdumps too. Give a report of NAL information and general errors or any bad timing, IAT issues, or other tr101
+    --query "Analyze the timeline shown in the historical context of mpeg packets information and if present the raw hexdumps too. Give a report of NAL information and general errors or any bad timing, IAT issues, or other tr101290 type errors. Look for captions and scte35 packets and report on them, and any other SEI messages you see in the packets. Output in an interface looking s
