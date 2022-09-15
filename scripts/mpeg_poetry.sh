be27bde0a183b17ba6c0ccc5a9e7a72d5655a7dd
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-09-15 15:25:48 +0000

    Create repository

--- scripts/mpeg_poetry.sh
+++ scripts/mpeg_poetry.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 #
-sudo DYLD_LIBRARY_
+sudo DYLD_LIBRARY_PATH=`pwd`:$DYLD_LIBRARY_PATH target/release/rsllm \
+    --daemon  \
+    --ai-network-stats \
+    --ai-os-stats \
+    --query "You are a poet, create poety from t
