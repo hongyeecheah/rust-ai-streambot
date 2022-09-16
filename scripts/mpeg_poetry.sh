c15091312d9749b99817d2608721acac2b5c58bd
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-09-16 20:06:42 +0000

    Minor style fixes.

--- scripts/mpeg_poetry.sh
+++ scripts/mpeg_poetry.sh
@@ -4,4 +4,5 @@ sudo DYLD_LIBRARY_PATH=`pwd`:$DYLD_LIBRARY_PATH target/release/rsllm \
     --daemon  \
     --ai-network-stats \
     --ai-os-stats \
-    --query "You are a poet, create poety from t
+    --query "You are a poet, create poety from this data above. Please output poetry with details of the packets." \
+    --system-prompt "you are a poet who  makes mpegts into poetry" $@
