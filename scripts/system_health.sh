8db7fd6434b6c3d5e05af7a1ff81f5c021fc4624
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-09-17 08:41:27 +0000

    Initial repository

--- scripts/system_health.sh
+++ scripts/system_health.sh
@@ -3,4 +3,4 @@
 
 DYLD_LIBRARY_PATH=`pwd`:$DYLD_LIBRARY_PATH target/release/rsllm \
     --daemon \
-    --query "Determine if the system is healthy or
+    --query "Determine if the system is healthy or sick, diagnose the issue if possible or give details about it. Use the historical view to see bigger trends of the system. draw a table of the current system metr
