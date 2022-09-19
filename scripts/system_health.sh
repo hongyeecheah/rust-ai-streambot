c2ee20e0582f9fd4de204c5ae673a9688bd1ad4c
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-09-19 12:43:08 +0000

    Bump to 0.2.3

--- scripts/system_health.sh
+++ scripts/system_health.sh
@@ -3,4 +3,5 @@
 
 DYLD_LIBRARY_PATH=`pwd`:$DYLD_LIBRARY_PATH target/release/rsllm \
     --daemon \
-    --query "Determine if the system is healthy or sick, diagnose the issue if possible or give details about it. Use the historical view to see bigger trends of the system. draw a table of the current system metr
+    --query "Determine if the system is healthy or sick, diagnose the issue if possible or give details about it. Use the historical view to see bigger trends of the system. draw a table of the current system metrics vs. historical showing changes over time." \
+    --system-prompt "you are able to say green or red depending on the system health determined f
