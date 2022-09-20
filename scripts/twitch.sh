328cb3a5aaaf39b8a110cd83c6869a2fb3a3b647
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-09-20 22:46:52 +0000

    OAK-3654 - Integrate with Metrics for various stats collection

--- scripts/twitch.sh
+++ scripts/twitch.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #
 # Alice's AI Wonderland Character:
-# - Parody of w
+# - Parody of walt-disney's original Alice animations, the first ones that got published.
+#
+# RsLLM configuration script:
+# - @2024 Christi Kennedy - This is not related 
