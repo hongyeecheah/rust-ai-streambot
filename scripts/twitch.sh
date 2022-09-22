b805db31439e25512c378a0d3478df705c4e27ad
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-09-22 07:37:39 +0000

    Updated Assamese translation

--- scripts/twitch.sh
+++ scripts/twitch.sh
@@ -4,4 +4,11 @@
 # - Parody of walt-disney's original Alice animations, the first ones that got published.
 #
 # RsLLM configuration script:
-# - @2024 Christi Kennedy - This is not related 
+# - @2024 Christi Kennedy - This is not related to any known alices or wonderlands.
+#
+#
+
+# === CONFIGURATION ===
+BUILD_TYPE=release
+## Interstitial message
+GREETING="Hi I'm Alice, ask me a question by typing '!me
