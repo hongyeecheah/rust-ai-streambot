f5e2bc3e847d0ea2561bb32a921b5d317780d76b
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-10-27 16:11:20 +0000

    * CodeJam.BenchmarkDotNet project - WORK IN PROGRESS

--- src/args.rs
+++ src/args.rs
@@ -95,3 +95,168 @@ pub struct Args {
         long,
         env = "MAX_TOKENS",
         default_value = "800",
+        help = "Max Tokens, 1 to N."
+    )]
+    pub max_tokens: i32,
+
+    /// Model
+    #[clap(
+        long,
+        env = "MODEL",
+        default_value = "no-model-specified",
+        help = "OpenAI LLM Model (N/A with local Llama2 based LLM)"
+    )]
+    pub model: String,
+
+    /// LLM Host url with protocol, host, port,  no path
+    #[clap(
+        long,
+        env = "LLM_HOST",
+        default_value = "http://127.0.0.1:8080",
+        help = "LLM Host url with protocol, host, port,  no path"
+    )]
+    pub llm_host: String,
+
+    /// LLM Url path
+    #[clap(
+        long,
+        env = "LLM_PATH",
+        default_value = "/v1/chat/completions",
+        help = "LLM Url path for completions."
+    )]
+    pub llm_path: String,
+
+    /// LLM History size
+    #[clap(
+        long,
+        env = "LLM_HISTORY_SIZE",
+        default_value = "16768",
+        help = "LLM History size (0 is unlimited)."
+    )]
+    pub llm_history_size: usize,
+
+    /// Clear History - clear the history of the LLM each iteration
+    #[clap(
+        long,
+        env = "CLEAR_HISTORY",
+        default_value = "false",
+        help = "Clear History - clear the history of the LLM each iteration."
+    )]
+    pub no_history: bool,
+
+    /// Interactive mode - command line input
+    #[clap(
+        long,
+        env = "INTERACTIVE",
+        default_value = "false",
+        help = "Interactive mode - command line input."
+    )]
+    pub interactive: bool,
+
+    /// Don't stream output
+    #[clap(
+        long,
+        env = "NO_STREAM",
+        default_value = "false",
+        help = "Don't stream output, wait for all completions to be generated before returning."
+    )]
+    pub no_stream: bool,
+
+    /// Safety feature for using openai api and confirming you understand the risks
+    #[clap(
+        long,
+        env = "USE_OPENAI",
+        default_value = "false",
+        help = "Safety feature for using openai api and confirming you understand the risks, you must also set the OPENAI_API_KEY, this will set the llm-host to api.openai.com."
+    )]
+    pub use_openai: bool,
+
+    /// MetaVoice as text to speech
+    #[clap(
+        long,
+        env = "METAVOICE_TTS",
+        default_value = "false",
+        help = "MetaVoice as text to speech."
+    )]
+    pub metavoice_tts: bool,
+
+    /// OAI_TTS as text to speech from openai
+    #[clap(
+        long,
+        env = "OAI_TTS",
+        default_value = "false",
+        help = "OAI_TTS as text to speech from openai."
+    )]
+    pub oai_tts: bool,
+
+    /// MIMIC3_TTS as text to speech from openai
+    #[clap(
+        long,
+        env = "MIMIC3_TTS",
+        default_value = "false",
+        help = "MIMIC3_TTS as text from mimic3-server local API."
+    )]
+    pub mimic3_tts: bool,
+
+    /// MIMIC3_VOICE voice model via text string to use for mimic3 tts, en_US/vctk_low#p326 is a good male voice
+    #[clap(
+        long,
+        env = "MIMIC3_VOICE",
+        default_value = "en_US/vctk_low#p303",
+        help = "MIMIC3_VOICE voice model via text string to use for mimic3 tts. Use en_US/vctk_low#p326 for a male voice, default is female."
+    )]
+    pub mimic3_voice: String,
+
+    /// TTS text to speech enable
+    #[clap(
+        long,
+        env = "TTS_ENABLE",
+        default_value = "false",
+        help = "TTS text to speech enable."
+    )]
+    pub tts_enable: bool,
+
+    /// audio chunk size
+    #[clap(
+        long,
+        env = "AUDIO_CHUNK_SIZE",
+        default_value = "1.0",
+        help = "audio chunk size in seconds for text to speech."
+    )]
+    pub audio_chunk_size: f32,
+
+    /// Pipeline concurrency - max concurrent pipeline tasks
+    #[clap(
+        long,
+        env = "PIPELINE_CONCURRENCY",
+        default_value = "1",
+        help = "Pipeline concurrency - max concurrent pipeline tasks."
+    )]
+    pub pipeline_concurrency: usize,
+
+    /// debug inline on output (can mess up the output) as a bool
+    #[clap(
+        long,
+        env = "DEBUG_INLINE",
+        default_value = "false",
+        help = "debug inline on output (can mess up the output) as a bool."
+    )]
+    pub debug_inline: bool,
+
+    /// Show output errors
+    #[clap(
+        long,
+        env = "SHOW_OUTPUT_ERRORS",
+        default_value = "false",
+        help = "Show LLM output errors which may mess up the output and niceness if packet loss occurs."
+    )]
+    pub show_output_errors: bool,
+
+    /// Monitor system stats
+    #[clap(
+        long,
+        env = "AI_OS_STATS",
+        default_value = "false",
+        help = "Monitor system stats."
+    )]
+    pub ai_os_stats: bool,
