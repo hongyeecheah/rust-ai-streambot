b2193a391af50a25d5c1a12bdb5e5185c7d746b0
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-11-01 09:28:37 +0000

    Small doc updates

--- src/audio.rs
+++ src/audio.rs
@@ -2,4 +2,10 @@ use minimp3::{Decoder, Frame};
 use std::io::Cursor;
 use std::io::Result;
 
-/// Converts WAV PCM dat
+/// Converts WAV PCM data to f32 samples without explicitly handling error cases in the return type.
+///
+/// # Arguments
+/// * `wav_data` - The bytes of a WAV file.
+///
+/// # Returns
+/
