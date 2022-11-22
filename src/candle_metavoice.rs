ff9993348e09d83d195dd3b0da776ae8ef1e65f8
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-11-22 10:21:56 +0000

    sync: optimize fence merges

--- src/candle_metavoice.rs
+++ src/candle_metavoice.rs
@@ -1,1 +1,9 @@
-#[cfg(feature = "mk
+#[cfg(feature = "mkl")]
+extern crate intel_mkl_src;
+
+#[cfg(feature = "accelerate")]
+extern crate accelerate_src;
+
+#[cfg(feature = "metavoice")]
+use anyhow::{Error, Result};
+#[cfg(feature = "m
