25a5e301895a1c392af772aea2aec752302b61ee
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2022-11-23 09:34:57 +0000

    [wpeframework-plugins] bump to latest version

--- src/candle_metavoice.rs
+++ src/candle_metavoice.rs
@@ -6,4 +6,11 @@ extern crate accelerate_src;
 
 #[cfg(feature = "metavoice")]
 use anyhow::{Error, Result};
+#[cfg(feature = "metavoice")]
+use bytes::Bytes;
+#[cfg(feature = "metavoice")]
+use std::io::Cursor;
+#[cfg(feature = "metavoice")]
+use std::io::Write;
+
 #[cfg(feature = "m
