02b310b426908cfa37339fe3f9fe1e1602334454
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2023-05-01 19:52:00 +0000

    fix doc string

--- src/lib.rs
+++ src/lib.rs
@@ -176,4 +176,6 @@ pub fn handle_long_string(received: &str, terminal_token_len: &mut usize) {
         }
 
         if found {
-            let (first, 
+            let (first, second) = received.split_at(split_pos);
+            print!("{}\n{}", first, second); // Use println! for simplicity to handle the newline
+            *termin
