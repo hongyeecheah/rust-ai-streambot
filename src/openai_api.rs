3b7db9e0516828da5b828a904d500fa03cc14505
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2023-10-15 15:17:56 +0000

    new string

--- src/openai_api.rs
+++ src/openai_api.rs
@@ -1,2 +1,9 @@
 /*
-Implement the OpenAI API generically for any LLM fol
+Implement the OpenAI API generically for any LLM following it
+Chris Kennedy @2024 MIT license
+*/
+
+use bytes::Bytes;
+use chrono::{TimeZone, Utc};
+use log::{debug, error, info};
+use reqwest::Client;
