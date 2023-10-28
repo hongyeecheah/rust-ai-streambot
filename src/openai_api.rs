2240c976edc4c0cb32cbff4e1ab6a84648de463d
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2023-10-28 10:13:35 +0000

    0.1.15

--- src/openai_api.rs
+++ src/openai_api.rs
@@ -75,4 +75,12 @@ pub fn format_messages_for_llm(messages: Vec<Message>, chat_format: String) -> S
         "[/INST]"
     } else if chat_format == "google" {
         "<end_of_turn>"
-    } else if chat_fo
+    } else if chat_format == "chatml" {
+        "<im_end>"
+    } else if chat_format == "vicuna" {
+        "\n"
+    } else {
+        ""
+    };
+    // Assistant Tokens
+    let assist_token = i
