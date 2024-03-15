982bf84d8bb92b99ed93fe83419fe00a16be0163
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2024-03-15 13:19:45 +0000

    Import from zlib-1.2.4

--- src/sd_automatic.rs
+++ src/sd_automatic.rs
@@ -1,4 +1,12 @@
 use crate::scale_image;
 use crate::stable_diffusion::SDConfig;
 use crate::stable_diffusion::StableDiffusionVersion;
-use anyhow:
+use anyhow::Result;
+use base64::engine::general_purpose;
+use base64::Engine;
+use image::ImageBuffer;
+use image::Rgb;
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+
+pub as
