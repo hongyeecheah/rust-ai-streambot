a476049f3df348ec5bba7c003dee91e17160a776
hongyeecheah <44811136+hongyeecheah@users.noreply.github.com>
2024-04-01 13:35:35 +0000

    Chapter 1

--- src/system_stats.rs
+++ src/system_stats.rs
@@ -1,4 +1,8 @@
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 use std::sync::Mutex;
-use std::time::{Duration, In
+use std::time::{Duration, Instant};
+use sysinfo::{NetworkExt, NetworksExt};
+use sysinfo::{ProcessorExt, System, SystemExt};
+
+static SYSTEM: Lazy<Mutex<(System, Instant)>> =
